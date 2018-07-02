4aaed9f63c558bd33873c049b1d113a8f564984d
liuhui <Filbuk.Liu@ncifangwangluo.com>
2018-07-02 11:15:29 +0800

    sdfs

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const os = require('os');
+const Koa = require('koa');
+const app = new Koa();
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('koa-bodyparser');
+const serve = require('koa-static');
+
+const main = serve(path.join(__dirname));
+app.use(main);
+// response
+app.use(bodyParser());
+console.log(os.networkInterfaces())
+var data = fs.readFileSync('input.txt'); 
+app.use(async (ctx, next) => {
+	
+    if (ctx.request.path === '/') {
+		console.log(ctx.request.origin)
+        ctx.response.body = fs.createReadStream('./demos/index.html');
+    } else {
+        await next();
+    }
+});
+
+app.use(async (ctx, next) => {
+    if (ctx.request.path === '/test') {
+		console.log(ctx.request.origin)
+        ctx.response.body = data.toString();
+    } else {
+        await next();
+    }
+});
+
+app.use(async (ctx, next) => {
+    if (ctx.request.path === '/error') {
+        ctx.response.body = 'ERROR page';
+    } else {
+        await next();
+    }
+}); 
+app.listen(2000);
